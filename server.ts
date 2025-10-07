90b3bd27d8393ca06c243fd6391a53cb4fafb972
Abdulaziz Alabduljalil <32106559+Wazyre@users.noreply.github.com>
2025-10-07 11:07:14 +0300

    Fix issue of npm not detecting package.json name

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
-import cors from 'cors';
 import { PrismaClient } from './generated/prisma/client.js';
 import userRoutes from './routes/userRoutes.ts';
 import supportRoutes from './routes/supportRoutes.ts';
@@ -17,19 +16,12 @@ const __dirname = path.resolve();
 const urlencodedParser = express.urlencoded({extended: false});
 app.use(urlencodedParser, express.json());
 
-
-// var corsOptions = {
-//   origin: 'http://localhost:5173',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-// app.use(cors());
-
 // Initialize Postgres ORM and DB
 const prisma = new PrismaClient();
 
 // Env variables
 const PORT = process.env.SERVER_PORT || 54322;
-const STATUS = process.env.NODE_ENV || 'developement'
+const STATUS = process.env.NODE_ENV || 'development'
 const DB_URI = process.env.DB_URI
 
 // Setup APIs
@@ -40,18 +32,11 @@ app.use('/api/support', supportRoutes);
 if(STATUS === 'production') {
   //Set static folder
   app.use(express.static('frontend/dist'));
-
-  app.get('*', (req, res) => {
+  // See https://expressjs.com/en/guide/migrating-5.html#path-syntax for splat explanation
+  app.get('/*splat', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   })
 }
-
-// // Connect to database
-// mongoose.connect(DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-// .then((res) => {
-//   console.log("Database connected");
-// })
-// .catch((err) => console.log(err));
 
 // Log if server is up
 app.listen(PORT, () => {
