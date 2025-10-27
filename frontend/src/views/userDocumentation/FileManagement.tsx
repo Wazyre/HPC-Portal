c54abde5db1f59c8450db7419ad2074bb10a5f8d
Abdulaziz Alabduljalil <32106559+Wazyre@users.noreply.github.com>
2025-10-27 14:44:40 +0300

    Change to Text from Box

--- frontend/src/views/userDocumentation/FileManagement.tsx
+++ frontend/src/views/userDocumentation/FileManagement.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardSection, Code, Container, Divider, Image, List, ListItem, Text, Title } from "@mantine/core";
+import {Card, CardSection, Code, Container, Divider, Image, List, ListItem, Text, Title } from "@mantine/core";
 import { useVerifyUser } from "../../utils/useVerifyUser";
 import { fileMngImgs } from "../../assets/images/filemng";
 
@@ -14,11 +14,16 @@ const FileManagement = () => {
                     <Text>File Permissions in Linux</Text>
                 </CardSection>
                 <Text>
-                    Like many other operating systems Linux uses a method of access rights on files and directories. Each file and directory has access rights that are associated with each one. The file permission normally shown on the left-hand side of each file/directory with 10-digit symbol (e.g. 
-                    <Box component="strong" >drwxr-xr-x</Box>):
+                    Like many other operating systems Linux uses a method of access rights on files and 
+                    directories. Each file and directory has access rights that are associated with each 
+                    one. The file permission normally shown on the left-hand side of each file/directory 
+                    with 10-digit symbol (e.g. <Text component="span" fw={700} >drwxr-xr-x</Text>):
                 </Text>
                 <List>
-                    <ListItem>The first letter presents whether the file is a directory or not “d: directory”. </ListItem>
+                    <ListItem>
+                        The first letter presents whether the file is a directory or not 
+                        “d: directory”. 
+                    </ListItem>
                     <ListItem>The next three represent the file owner permission</ListItem>
                     <ListItem>The next three represent the file group permission</ListItem>
                     <ListItem>The last three represent the file permissions for everyone else.</ListItem>
@@ -28,9 +33,18 @@ const FileManagement = () => {
                     For each of the permission parts the letters mean the following in their groups:
                 </Text>
                 <List>
-                    <ListItem><Box component="strong">r:</Box> Read permission to read and copy the file, its absence indicates this is not available.</ListItem>
-                    <ListItem><Box component="strong">w:</Box> Write permission to write the file, its absence indicates this is not available.</ListItem>
-                    <ListItem><Box component="strong">x:</Box> Execution permission to allow the file to be executed, its absence indicates this is not available.</ListItem>
+                    <ListItem>
+                        <Text component="span" fw={700}>r:</Text> Read permission to read and copy 
+                        the file, its absence indicates this is not available.
+                    </ListItem>
+                    <ListItem>
+                        <Text component="span" fw={700}>w:</Text> Write permission to write the file, its 
+                        absence indicates this is not available.
+                    </ListItem>
+                    <ListItem>
+                        <Text component="span" fw={700}>x:</Text> Execution permission to allow the file 
+                        to be executed, its absence indicates this is not available.
+                    </ListItem>
                 </List>
                 
                 <Text fw={700} mt={20}>Example 1</Text>
@@ -39,7 +53,10 @@ const FileManagement = () => {
                     Using the example above would mean:
                 </Text>
                 <List listStyleType="square">
-                    <ListItem>“job1” and “job4” are directories, user “hpcdemo” is the owner and have full access (read/write/execute), users in “research” group have full access too and everyone else have read and execute permission only.</ListItem>
+                    <ListItem>“job1” and “job4” are directories, user “hpcdemo” is the owner and have full 
+                        access (read/write/execute), users in “research” group have full access too and 
+                        everyone else have read and execute permission only.
+                    </ListItem>
                     <ListItem>“job2” and “job3” are directories, user “hpcdemo” is the owner and have full access (read/write/execute), users in “hpcdemo” group have full access too and everyone else have read and execute permission only.</ListItem>
                 </List>
                 
@@ -61,17 +78,17 @@ const FileManagement = () => {
                 <Text>To change the file (and directory) permissions use the command line “chmod” considering the following:</Text>
                 <Text>Linux file permissions are managed using four categories:</Text>    
                 <List>
-                    <ListItem><Box component="strong">User (u):</Box> The owner of the file or directory.</ListItem>
-                    <ListItem><Box component="strong">Group (g):</Box> The group associated with the file or directory.</ListItem>
-                    <ListItem><Box component="strong">Others (o):</Box> All other users on the system.</ListItem>
-                    <ListItem><Box component="strong">All (a):</Box> All the users on the system</ListItem>
+                    <ListItem><Text component="span" fw={700}>User (u):</Text> The owner of the file or directory.</ListItem>
+                    <ListItem><Text component="span" fw={700}>Group (g):</Text> The group associated with the file or directory.</ListItem>
+                    <ListItem><Text component="span" fw={700}>Others (o):</Text> All other users on the system.</ListItem>
+                    <ListItem><Text component="span" fw={700}>All (a):</Text> All the users on the system</ListItem>
                 </List> 
 
                 <Text mt={10}>Each category can have three types of permissions:</Text> 
                 <List>
-                    <ListItem><Box component="strong">Read (r):</Box> Allows viewing the contents of a file or listing the contents of a directory.</ListItem>
-                    <ListItem><Box component="strong">Write (w):</Box> Allows modifying the contents of a file or creating/deleting files within a directory.</ListItem>
-                    <ListItem><Box component="strong">Execute (x):</Box> Allows running a file (if it's an executable) or entering a directory.</ListItem>
+                    <ListItem><Text component="span" fw={700}>Read (r):</Text> Allows viewing the contents of a file or listing the contents of a directory.</ListItem>
+                    <ListItem><Text component="span" fw={700}>Write (w):</Text> Allows modifying the contents of a file or creating/deleting files within a directory.</ListItem>
+                    <ListItem><Text component="span" fw={700}>Execute (x):</Text> Allows running a file (if it's an executable) or entering a directory.</ListItem>
                 </List>   
 
                 <Text mt={10}>With the permission action:</Text>
@@ -97,20 +114,20 @@ const FileManagement = () => {
                 {/* -------------------------------------------------------- */}
 
                 <Text size="xl" mt={20}>File Compression</Text>
-                <Text>The <Box component="strong">gzip</Box> command is used to compress the files to take up less space and fast in transferring them.</Text>
+                <Text>The <Text component="span" fw={700}>gzip</Text> command is used to compress the files to take up less space and fast in transferring them.</Text>
                 <Code block>[hpcdemo@clavis1 ~]$ gzip filname.c</Code>
 
-                <Text mt={20}>The <Box component="strong">gunzip</Box> command is used to unzip the compressed file.</Text>
+                <Text mt={20}>The <Text component="span" fw={700}>gunzip</Text> command is used to unzip the compressed file.</Text>
                 <Code block>[hpcdemo@clavis1 ~]$ gunzip filname.cgz</Code>
 
-                <Text mt={20}>The <Box component="strong">tar</Box> command is used to create archives, which are single files that contain a collection of other files and directories. It has several options:</Text>
+                <Text mt={20}>The <Text component="span" fw={700}>tar</Text> command is used to create archives, which are single files that contain a collection of other files and directories. It has several options:</Text>
                 <List>
-                    <ListItem><Box component="strong">-c:</Box> Create a new archive</ListItem>
-                    <ListItem><Box component="strong">-z:</Box> Use gzip compression</ListItem>
-                    <ListItem><Box component="strong">-v:</Box> Verbose mode (shows files being processed)</ListItem>
-                    <ListItem><Box component="strong">-f:</Box> Specify the archive filename</ListItem>
-                    <ListItem><Box component="strong">-x:</Box> Extract files from an archive</ListItem>
-                    <ListItem><Box component="strong">-t:</Box> List the contents of an archive</ListItem>
+                    <ListItem><Text component="span" fw={700}>-c:</Text> Create a new archive</ListItem>
+                    <ListItem><Text component="span" fw={700}>-z:</Text> Use gzip compression</ListItem>
+                    <ListItem><Text component="span" fw={700}>-v:</Text> Verbose mode (shows files being processed)</ListItem>
+                    <ListItem><Text component="span" fw={700}>-f:</Text> Specify the archive filename</ListItem>
+                    <ListItem><Text component="span" fw={700}>-x:</Text> Extract files from an archive</ListItem>
+                    <ListItem><Text component="span" fw={700}>-t:</Text> List the contents of an archive</ListItem>
                 </List>
                 
                 <Text mt={20}>To create a compressed archive:</Text>
